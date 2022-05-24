eb47f84685217707ec2ec8e7ce379aaaea402d18
Kimon Kremizas <kimon@shape.dk>
2022-05-25 01:19:40 +0200

    Failed test

--- app/src/test/java/GetDayEndingTest.kt
+++ app/src/test/java/GetDayEndingTest.kt
@@ -11,7 +11,7 @@ class GetDayEndingTest {
     @Test
     @Throws(Exception::class)
     fun getDayEnding_valid_second() {
-        assertEquals("nd", GetDayEnding(2))
+        assertEquals("nt", GetDayEnding(2))
     }
     @Test
     @Throws(Exception::class)
